72989946ebb41576fc21ea449a7164c8543f3946
Xavier Lambein <xlambein@gmail.com>
2023-01-29 22:14:44 +0100

    Add Mastodon rel=me

--- templates/base.html.nix
+++ templates/base.html.nix
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css"
       integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">
+    <link rel="me" href="https://vis.social/@xlambein" />
   </head>
 
   <body>

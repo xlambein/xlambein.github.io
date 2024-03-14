fbf5e686691bedcd06bd978479f0d9fb32058343
Xavier Lambein <xavier@lambein.xyz>
2024-03-14 13:13:52 +0100

    Fix `phrase-generator` page missing

--- pages/phrase-generator/default.nix
+++ pages/phrase-generator/default.nix
@@ -1,18 +1,22 @@
-{ callPackage
-, fetchFromGitHub
-, ...
-}:
-
-let phrase-generator = fetchFromGitHub {
-  owner = "xlambein";
-  repo = "phrase-generator";
-  rev = "0c68228bd07725c0d55f57a6a9b0c047bad6bc2f";
-  sha256 = "sha256-ps1Uvi98JlZ5XXM8Bd31s8wXbWtPm8HGD7eqO/YwNX0=";
-}; in
-(callPackage phrase-generator {}).overrideAttrs (attrs: {
-  passthru.metadata = {
-    title = "Phrase Generator";
-    pubdate = "2022-01-12";
-    tags = [ "elm" ];
+{
+  callPackage,
+  fetchFromGitHub,
+  nixss,
+  ...
+}: let
+  phrase-generator = fetchFromGitHub {
+    owner = "xlambein";
+    repo = "phrase-generator";
+    rev = "0c68228bd07725c0d55f57a6a9b0c047bad6bc2f";
+    sha256 = "sha256-ps1Uvi98JlZ5XXM8Bd31s8wXbWtPm8HGD7eqO/YwNX0=";
   };
-})
+in
+  nixss.util.derivation {
+    drv = callPackage phrase-generator {};
+    filename = "phrase-generator";
+    metadata = {
+      title = "Phrase Generator";
+      pubdate = "2022-01-12";
+      tags = ["elm"];
+    };
+  }

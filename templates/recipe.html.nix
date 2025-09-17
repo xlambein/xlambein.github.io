57d2e6ebbdbccbefb19de648c5f6b8c3df50883d
Xavier Lambein <xavier@lambein.xyz>
2025-09-17 15:50:27 +0200

    Add JS script to scale recipes

--- templates/recipe.html.nix
+++ templates/recipe.html.nix
@@ -20,6 +20,14 @@
     ''
     else ""
   }
+
+      <div class="article-info">
+        Scale ingredients:
+        <label><input type="radio" name="scale" onclick="javascript:scaleAll(1)" checked />1</label>
+        <label><input type="radio" name="scale" onclick="javascript:scaleAll(2)" />2</label>
+        <label><input type="radio" name="scale" onclick="javascript:scaleAll(3)" />3</label>
+        <label><input type="radio" name="scale" onclick="javascript:scaleAll(4)" />4</label>
+      </div>
     </header>
 
     ${content}
@@ -36,4 +44,64 @@
   }
 
   </article>
+
+  <script>
+    const reQuantity = /^\d+(?:\/\d+)?/d;
+
+    function gcd(a, b) {
+      a = Math.abs(a);
+      b = Math.abs(b);
+      if (b > a) {const temp = a; a = b; b = temp;}
+      while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
+      }
+    }
+
+    function scale(text, factor) {
+      const match = reQuantity.exec(text);
+      if (match === null) return;
+
+      const quantity = match[0];
+
+      let scaled;
+      if (Number.isNaN(+quantity)) {
+        let [n, d] = quantity.split("/");
+        n = +n * factor;
+        d = +d;
+        const g = gcd(n, d);
+        n = n / g;
+        d = d / g;
+        scaled = "" + n;
+        if (d != 1) scaled = scaled + "/" + d
+      } else {
+        scaled = "" + (factor * (+quantity));
+      }
+
+      return scaled + text.substr(quantity.length);
+    }
+
+    const lis = document.querySelector("article").querySelectorAll("ul>li");
+
+    function prepareForScale() {
+      for (const li of lis) {
+        li.setAttribute("data-text", li.textContent);
+      }
+    }
+
+    prepareForScale();
+
+    function scaleLi(li, factor) {
+      const text = li.getAttribute("data-text");
+      const scaled = scale(text, factor);
+      if (scaled === undefined) return;
+      li.textContent = scaled;
+    }
+
+    function scaleAll(factor) {
+      for (const li of lis) scaleLi(li, factor);
+    }
+  </script>
 ''
